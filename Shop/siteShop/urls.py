fe6c92f4589ee3924a946422e7f3dad7a217feac
luciano <lucianojuniorr83@gmail.com>
2023-08-23 22:10:21 -0300

    mudanças feitas

--- Shop/siteShop/urls.py
+++ Shop/siteShop/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     path("", views.index, name='index'),
+    path("carrinho.html", views.carrinho, name='carrinho'),
+    path("produtos.html", views.produtos, name= 'produto'),
+    path("questions", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
--- Shop/siteShop/views.py
+++ Shop/siteShop/views.py
@@ -5,3 +5,20 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+def carrinho(request):
+    return render(request, 'carrinho.html')
+
+def produtos(request):
+    return render(request, 'produtos.html')
+
+def detail(request, question_id):
+    return HttpResponse("Você esta procurando" % question_id)
+
+def results(request, question_id):
+    response = "Você encontrou os resultados das perguntas %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você votou nas perguntas %s." % question_id)
+
